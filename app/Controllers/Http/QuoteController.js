e100a92b3d4020226bef94c9f0c4893e0a4bbf86
Alpha Olomi <alphaolomi@gmail.com>
2019-12-09 19:03:51 +0300

    fix(controller): change typo

--- app/Controllers/Http/QuoteController.js
+++ app/Controllers/Http/QuoteController.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const Quote = use('App/Models/Quote')
+
+
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
