3cab55673d2155cf9877536b7f49def1c65a1f12
Alpha Olomi <alphaolomi@gmail.com>
2019-12-09 18:43:55 +0300

    feat(models):add qoutes model and relation

--- app/Models/User.js
+++ app/Models/User.js
@@ -34,6 +34,10 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  quote () {
+    return this.hasMany('App/Models/Quote')
+  }
 }
 
 module.exports = User
